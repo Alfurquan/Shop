97954b33cb4678f40528ffe87d6f7efca20d5252
Alfurquan <zahedialfurquan20@gmail.com>
2020-03-10 23:16:30 +0530

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -7,4 +7,32 @@ $(document).ready(function() {
   $(".dropdown__heading").click(function() {
     $("#sort-menu").slideToggle();
   });
+  $(".partnersLogoSlider").slick({
+    slidesToShow: 4,
+    arrows: false,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        },
+
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        },
+
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  });
 });
